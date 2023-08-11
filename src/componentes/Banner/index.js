d1bb38e96e97835ead5958fe23db550561b057c3
ItamaraFerreiraA <itamaraaferreira100@gmail.com>
2023-08-11 08:36:34 -0300

    Criando o banner

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,12 @@
+
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <header className="banner">
+            <h1>Luminus Beauty</h1>
+        </header>
+    )   
+}    
+
+export default Banner
